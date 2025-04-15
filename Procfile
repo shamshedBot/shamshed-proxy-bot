9e40d7f8c5f926ef20e7be7bb6b1803607d6e4be
shamshedBot <mdshamshedhaider@gmail.com>
2025-04-15 12:31:39 +0600

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python shamshed_bot.py
---
+++ shamshed_bot.py
@@ -0,0 +1,30 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+
+BOT_TOKEN = "7696979981:AAFFmynvUB4AtEuMlo7IOZNI_YDzgF1XmA8"
+OWNER_ID = 8114177038
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(commands=["start"])
+async def start_handler(msg: Message):
+    if msg.from_user.id != OWNER_ID:
+        await msg.answer("🚫 You are not authorized to use this bot.")
+        return
+    await msg.answer("""🔥 Welcome to Shamshed Proxy Checker Bot
+If you need contact click @Shamshed_Boss
+
+Available Commands:
+1. /start
+2. /check
+3. /live
+4. /dead.""")
+
+async def main():
+    await asyncio.sleep(3)  # Delay to avoid Telegram conflicts
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
